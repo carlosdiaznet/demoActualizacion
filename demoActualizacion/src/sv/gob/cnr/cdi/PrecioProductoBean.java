fd7e7f3cf06d57eeaf5bd40689ce9a730dafbeb0
juan.diaz <juan.diaz@DTI-W7JDIAZ.cnr.gob.sv>
2018-10-12 16:00:26 -0600

    Se intento con EJB y SessionScoped

--- demoActualizacion/src/sv/gob/cnr/cdi/PrecioProductoBean.java
+++ demoActualizacion/src/sv/gob/cnr/cdi/PrecioProductoBean.java
@@ -1,24 +1,31 @@
 package sv.gob.cnr.cdi;
 
 
+import java.io.Serializable;
+
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 import sv.gob.cnr.service.CalculadoraPrecio;
 
 @Named("precioMB")
-public class PrecioProductoBean {
+@SessionScoped
+public class PrecioProductoBean implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+
 	private double precio;
 	
-	@Inject
+	@EJB
 	private CalculadoraPrecio calculadora;
 	
 
 	@PostConstruct
 	public void init(){
 		System.out.println("Iniciando init");
+		this.calculadora = new CalculadoraPrecio();
 	}
 
 	public double getPrecio() {
